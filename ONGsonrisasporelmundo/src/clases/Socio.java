ea6878c3737bfcf1e39cd4be9a9149a4ac7f7bd3
Croixiosx <adriam.webs@gmail.com>
2021-10-17 19:26:42 +0200

    Creación clase Socio

---
+++ ONGsonrisasporelmundo/src/clases/Socio.java
@@ -0,0 +1,32 @@
+package clases;
+
+public class Socio {
+	
+	private int idPartner;
+	private String namePartner;
+	
+	//Constructors
+	
+	public Socio() {
+	}
+
+	public Socio(int idPartner, String namePartner) {
+		this.idPartner = idPartner;
+		this.namePartner = namePartner;
+	}
+
+	//Getter y Setter
+	public int getIdPartner() {
+		return idPartner;
+	}
+	public void setIdPartner(int idPartner) {
+		this.idPartner = idPartner;
+	}
+	public String getNamePartner() {
+		return namePartner;
+	}
+	public void setNamePartner(String namePartner) {
+		this.namePartner = namePartner;
+	}
+	
+}
